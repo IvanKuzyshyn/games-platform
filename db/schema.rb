dcdc5e2d5d2178ef310c856046e39939e49bd626
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2019-09-09 20:18:15 +0300

    Update active record schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_07_115155) do
+ActiveRecord::Schema.define(version: 2019_05_26_104420) do
+
+  create_table "results", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_results_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
