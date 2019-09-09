04528326e443c9f30e71bc70cbc20499c36dccb2
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2019-09-09 20:18:00 +0300

    Update user model and type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,7 +2,7 @@ module Types
   class UserType < GraphQL::Schema::Object
     graphql_name 'User'
 
-    field :id, ID, null: false
+    field :id, Integer, null: false
     field :name, String, null: false
     field :created_at, String, null: false
     field :updated_at, String, null: false
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :result
+
   validates_uniqueness_of :name
 end
