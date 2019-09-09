724a264af8adedf20658585106f7d07de934bdbc
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2019-09-09 20:17:48 +0300

    Update query and mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < GraphQL::Schema::Object
+    field :writeResults, mutation: Mutations::WriteResults
     field :createUser, mutation: Mutations::CreateUser
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,8 +3,15 @@ module Types
     field :user, Types::UserType, null: true do
       argument :name, String, required: true
     end
+
+    field :results, [Types::ResultType, null: true], null: false
+
     def user(name:)
       User.find_by({name: name})
+    end
+
+    def results
+      Result.all
     end
   end
 end
